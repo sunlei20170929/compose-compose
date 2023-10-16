1c70ad25eaae5c45f217114f1852f10ccc6010fc
sunlei <sunlei20170929@gmail.com>
2023-10-16 15:42:35 +0800

    add layout package

---
+++ app/src/main/java/com/example/compose_compose/layout/lazyLayout.kt
@@ -0,0 +1,13 @@
+package com.example.compose_compose.layout
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun lazyLayout(modifier: Modifier) {
+
+    /**
+     * lazylist ,lazycolumn,lazyverticalgrid,lazyhorizontalgrid,
+     * LazyVerticalStaggeredGrid and LazyHorizontalStaggeredGrid
+     * */
+}
